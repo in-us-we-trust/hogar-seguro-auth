44bf60be59784a0af6cf508c4434313b12da2ac8
kevin <kvallejop@fi.uba.ar>
2026-04-19 06:24:06 -0300

    inicializando app auth

---
+++ src/test/java/ar/edu/uba/hogar/auth/AuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.uba.hogar.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
